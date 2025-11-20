e3aa2f03479191402273eafaeb377a8bfd8fe114
Hilman Fauzi <h.fauzi0211@gmail.com>
2025-11-20 17:53:13 +0900

    fragment

--- app/src/main/java/com/example/musicchords/MainActivity.java
+++ app/src/main/java/com/example/musicchords/MainActivity.java
@@ -7,18 +7,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.musicchords.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        switchFragment(new HomeFragment());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        binding.navMenu.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home){
+                switchFragment(new HomeFragment());
+            }else {
+                switchFragment(new HistoryFragment());
+            }
+            return true;
+        });
+    }
+    private void switchFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
     }
 }
